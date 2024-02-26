3e9f7bcda77733e942c808fb19ec6952b87f97bd
Talal Salman Zafeer <mustangcreator@gmail.com>
2024-02-26 19:26:12 +0300

    Authentication Blocs created

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:user_repository/user_repository.dart';
+import 'blocs/authentication_bloc/authentication_bloc.dart';
 
 class MyApp extends StatelessWidget {
   final UserRepository userRepository;
