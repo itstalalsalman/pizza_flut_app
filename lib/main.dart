bd1337642f02ae2f2a2d0f10b605fd065eb5b53e
Talal Salman Zafeer <mustangcreator@gmail.com>
2024-02-26 18:32:23 +0300

    User Repository to store user information and initiating the login-logout UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pizza_flut_app/app.dart';
+import 'package:pizza_flut_app/simple_bloc_observer.dart';
+import 'package:user_repository/user_repository.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Bloc.observer = SimpleBlocObserver();
+  runApp(MyApp(FirebaseUserRepo()));
 }
 
--- packages/user_repository/lib/user_repository.dart
+++ packages/user_repository/lib/user_repository.dart
@@ -1,4 +1,7 @@
 library user_repository;
 
 
-export '';
+export 'src/entities/entities.dart';
+export 'src/models/models.dart';
+export 'src/user_repo.dart';
+export 'src/firebase_user_repo.dart';
